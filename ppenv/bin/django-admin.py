eba2a4929b20c84d91a9eb9a8b1209b0c36d27dc
Larry Laski <larry.laski@progyny.com>
2019-03-26 16:38:45 -0400

    docker and django setup

---
+++ ppenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/larry.laski/Sites/Education/django-python-advanced/ppenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
